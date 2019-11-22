9f337d152b5b097af7e8fad05688c1b53f39acf1
Kamil <zekse00@gmail.com>
2019-11-22 09:18:58 +0100

    f

---
+++ navbar.js
@@ -0,0 +1,3 @@
+let nav= '<a class="nav-link active" href="index.html">Home</a><a class="nav-link active" href="kontakt.html">Kontakt</a>'
+let elem = document.querySelector(".navbar");
+elem.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,9 @@
+
+//var btn=document.getElementById('btn');
+//btn.addEventListener('click', function()
+//if(btn.classList.contains('color')
+
+
+    function color(){
+        var btn = document.getElementById("btn");
+       btn.style.backgroundColor = "green";}
